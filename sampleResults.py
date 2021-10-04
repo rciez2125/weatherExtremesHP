60723df97b6e6e60b235294b087b825dd3059211
rciez2125 <rebecca.ciez@gmail.com>
2021-10-04 16:01:10 -0400

    updated state data (added MN)

--- sampleResults.py
+++ sampleResults.py
@@ -7,13 +7,15 @@ from tabulate import tabulate
 # make a plot of natural gas heating use for new york
 #df = censusdata.download('acs5', 2019, censusdata.censusgeo([('state', '36'), ('county', '*')]), ['B25040_001E', 'B25040_001M', 'B25040_002E', 'B25040_002M'])
 # state numbers are off (higher) by 4 from an alphabetical list ¯\_(ツ)_/¯
-df = censusdata.download('acs5', 2019, censusdata.censusgeo([('state', '27'), ('county', '*')]), ['B25040_001E', 'B25040_001M', 'B25040_002E', 'B25040_002M'])
-column_names = ['total_housing', 'housing_error', 'totalGas', 'gasError']
+df = censusdata.download('acs5', 2019, censusdata.censusgeo([('state', '27'), ('county', '*')]), ['B25040_001E', 'B25040_001M', 'B25040_002E', 'B25040_002M', 'B25040_004E', 'B25040_004M'])
+print(df.head())
+column_names = ['total_housing', 'housing_error', 'totalGas', 'gasError', 'totalElec', 'elecError']
 df.columns = column_names
 #df.to_csv('nystatedata.csv')
 df.to_csv('mnstatedata.csv')
 
 df['percentGas'] = df.apply(lambda row: row['totalGas']/row['total_housing'], axis = 1)
+df['percentElec'] = df.apply(lambda row: row['totalElec']/row['total_housing'], axis = 1)
 new_indices = []
 county_names = []
 
@@ -29,6 +31,12 @@ df['county_name'] = county_names
 
 import plotly.figure_factory as ff
 fig = ff.create_choropleth(fips = df.index, scope = ['Minnesota'], values = df.percentGas.values, binning_endpoints=(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9), title = 'Percentage of Homes with Natural Gas Heating Fuel', legend_title = 'Percentage using Natural Gas', simplify_county = 0.005)# colorscale = [[0, 'rgb(0,0,255)'], [1, 'rgb(255,0,0)']])
+#fig = ff.create_choropleth(fips = df.index, scope = ['New York'], values = df.percentGas.values, binning_endpoints=(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9), title = 'Percentage of Homes with Natural Gas Heating Fuel', legend_title = 'Percentage using Natural Gas', simplify_county = 0.005)# colorscale = [[0, 'rgb(0,0,255)'], [1, 'rgb(255,0,0)']])
+fig.layout.template = None
+fig.show()
+
+import plotly.figure_factory as ff
+fig = ff.create_choropleth(fips = df.index, scope = ['Minnesota'], values = df.percentElec.values, binning_endpoints=(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9), title = 'Percentage of Homes with Electric Heating', legend_title = 'Percentage using Electric Heat', simplify_county = 0.005)# colorscale = [[0, 'rgb(0,0,255)'], [1, 'rgb(255,0,0)']])
 #fig = ff.create_choropleth(fips = df.index, scope = ['New York'], values = df.percentGas.values, binning_endpoints=(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9), title = 'Percentage of Homes with Natural Gas Heating Fuel', legend_title = 'Percentage using Natural Gas', simplify_county = 0.005)# colorscale = [[0, 'rgb(0,0,255)'], [1, 'rgb(255,0,0)']])
 fig.layout.template = None
 fig.show()
